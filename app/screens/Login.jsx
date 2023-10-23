29eed12f81a0c6fc61f4638cbb59ebeb4cdc7246
monrealljeriko <monreall.jeriko@gmail.com>
2023-10-23 18:37:04 +0800

    Add AsyncStorage to save login details, initial masking for account

--- app/screens/Login.jsx
+++ app/screens/Login.jsx
@@ -7,7 +7,7 @@ import {
    TouchableOpacity,
 } from "react-native";
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Ionicons } from "@expo/vector-icons";
 import Checkbox from "expo-checkbox";
 import Button from "../component/Button";
@@ -17,14 +17,27 @@ import styles from "./styles";
 import { FIREBASE_AUTH } from "../../firebaseConfig";
 import { ActivityIndicator } from "react-native-paper";
 import { signInWithEmailAndPassword } from "firebase/auth";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 function Login({ navigation }) {
    const [isPasswordShown, setIsPasswordShown] = useState(true);
    const [isChecked, setIsChecked] = useState(false);
+   const [originalEmail, setOriginalEmail] = useState("");
    const [email, setEmail] = useState("");
    const [password, setPassword] = useState("");
    const [loading, setLoading] = useState(false);
    const auth = FIREBASE_AUTH;
+
+   /* const clearData = async () => {
+      // Clear saved login details in AsyncStorage
+      await AsyncStorage.removeItem("email");
+      await AsyncStorage.removeItem("password");
+      setOriginalEmail("");
+      setEmail("");
+      setPassword("");
+      setIsChecked(false);
+      console.log("Email and password data cleared");
+   }; */
 
    const signIn = async () => {
       setLoading(true);
@@ -35,6 +48,19 @@ function Login({ navigation }) {
             password
          );
 
+         // Check if the "Remember Me" checkbox is checked
+         if (isChecked) {
+            // Save the email and password to AsyncStorage
+            await AsyncStorage.setItem("email", email);
+            await AsyncStorage.setItem("password", password);
+            console.log("Email and password saved");
+         } else {
+            // If not checked, remove any saved login details
+            await AsyncStorage.removeItem("email");
+            await AsyncStorage.removeItem("password");
+            console.log("Email and password removed");
+         }
+
          console.log(response);
       } catch (error) {
          console.log(error);
@@ -43,6 +69,33 @@ function Login({ navigation }) {
          setLoading(false);
       }
    };
+
+   useEffect(() => {
+      // Check if there are saved login details in AsyncStorage
+      AsyncStorage.getItem("email").then((storedEmail) => {
+         if (storedEmail) {
+            setEmail(storedEmail);
+            /* setOriginalEmail(storedEmail);
+
+            // Mask the email for display
+            const [username, domain] = storedEmail.split("@");
+            const maskedUsername =
+               username.substring(0, 3) + "*".repeat(username.length - 3);
+            const maskedEmail = maskedUsername + "@" + domain;
+            setEmail(maskedEmail); */
+            setIsChecked(true); // Assuming the checkbox should be checked
+         }
+         // console.log("original ", originalEmail);
+
+         console.log("email", email);
+      });
+      AsyncStorage.getItem("password").then((storedPassword) => {
+         if (storedPassword) {
+            setPassword(storedPassword);
+         }
+      });
+   }, []);
+
    return (
       <View style={styles.container}>
          <Image
@@ -145,6 +198,7 @@ function Login({ navigation }) {
                   <Text style={styles.pressableText}>Click here</Text>
                </Pressable>
             </View>
+            {/* <Button title="Clear Async Data" onPress={clearData} /> */}
          </View>
       </View>
    );
